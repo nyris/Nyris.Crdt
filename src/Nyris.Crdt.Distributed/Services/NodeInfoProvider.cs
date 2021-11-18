685d2cbfa2e3ff2ada3baea4304b381b0b7ae1b1
Nikita Chizhov <nikita@nyris.io>
2021-11-18 02:26:35 +0100

    Initialize nodeId only once

--- src/Nyris.Crdt.Distributed/Services/NodeInfoProvider.cs
+++ src/Nyris.Crdt.Distributed/Services/NodeInfoProvider.cs
@@ -8,17 +8,22 @@ namespace Nyris.Crdt.Distributed.Services
 {
     internal static class NodeInfoProvider
     {
+        private static readonly NodeId NodeId = NodeId.FromGuid(Guid.NewGuid());
+        private static NodeInfo? _info;
+
         public static NodeInfo GetMyNodeInfo()
         {
+            if (_info != null) return _info;
+
             var host = Dns.GetHostEntry(Dns.GetHostName());
 
             foreach (var ip in host.AddressList)
             {
                 if (ip.AddressFamily == AddressFamily.InterNetwork)
                 {
-                    var nodeId = NodeId.FromGuid(Guid.NewGuid());
-                    Console.WriteLine($"This node was assigned id={nodeId}");
-                    return new NodeInfo(new Uri($"http://{ip}:{8080}"), nodeId);
+                    Console.WriteLine($"This node was assigned id={NodeId}");
+                    _info = new NodeInfo(new Uri($"http://{ip}:{8080}"), NodeId);
+                    return _info;
                 }
             }
 
