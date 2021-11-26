e39c28e0ca240aeb1ba7c0710adf84394964d686
Nikita Chizhov <nikita@nyris.io>
2021-11-26 17:31:48 +0100

    Mark ImageInfo as ProtoContract

--- samples/Nyris.Crdt.AspNetExample/ImageInfo.cs
+++ samples/Nyris.Crdt.AspNetExample/ImageInfo.cs
@@ -1,6 +1,8 @@
 using System;
+using ProtoBuf;
 
 namespace Nyris.Crdt.AspNetExample
 {
-    public sealed record ImageInfo(Uri DownloadUrl, string ImageId);
+    [ProtoContract(SkipConstructor = true)]
+    public sealed record ImageInfo([property: ProtoMember(1)] Uri DownloadUrl, [property: ProtoMember(2)] string ImageId);
 }
