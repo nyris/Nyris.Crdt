7c3cfcab0a2aa74e50ac00ef2114f8560f9d802d
Nikita Chizhov <nikita@nyris.io>
2022-03-15 19:10:33 +0100

    Remove unused code

--- src/Nyris.Crdt.Distributed/Model/WithId.cs
+++
@@ -1,7 +0,0 @@
-using ProtoBuf;
-
-namespace Nyris.Crdt.Distributed.Model
-{
-    [ProtoContract(SkipConstructor = true)]
-    public record WithId<T>([property: ProtoMember(1)] string Id, [property: ProtoMember(2)] T? Value);
-}
